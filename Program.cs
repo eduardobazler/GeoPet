d7f2c388d3012f9b444936d35ca57f4104a62a49
Adriano Santos <adrianosantos@Adrianos-MacBook-Air.local>
2022-12-08 01:07:53 -0300

    feat(Program): Included AddHttpClient

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using GeoPet.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient<IGeoPetService, GeoPetService>();
 
 var app = builder.Build();
 
@@ -22,3 +25,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
--- Services/GeoPetService.cs
+++ Services/GeoPetService.cs
@@ -3,7 +3,7 @@ namespace GeoPet.Services
     public class GeoPetService : IGeoPetService
     {
         private readonly HttpClient _client;
-        private const string _url = "https://nominatim.openstreetmap.org/";
+        private const string _url = "https://nominatim.openstreetmap.org/ui/";
         public GeoPetService(HttpClient client) 
         {
             _client = client;
@@ -12,7 +12,7 @@ namespace GeoPet.Services
 
         public async Task<object> FindGeoPet(string latitude, string longitude)
         {
-            var _resp = await _client.GetAsync($"reverse?format=jsonV2&lat={latitude}&lon={longitude}");
+            var _resp = await _client.GetAsync($"reverse?format=json&lat={latitude}&lon={longitude}");
             if(!_resp.IsSuccessStatusCode) return default!;
             var _res = await _resp.Content.ReadFromJsonAsync<object>();
             if (_res!.ToString() == "[]") return false;
