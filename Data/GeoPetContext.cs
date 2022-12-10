a7166ed5464bda6874343dbb36e0c137af856718
Adriano Santos <adrianosantos@Adrianos-MacBook-Air.local>
2022-12-10 03:31:20 -0300

    feat(GeoPet): Implemented migration, Values initial to GeoPetcontext and change ConnectionString

--- Data/GeoPetContext.cs
+++ Data/GeoPetContext.cs
@@ -14,8 +14,36 @@ public class GeoPetContext : DbContext, IGeoPetContext
     {
         if (!optionsBuilder.IsConfigured)
         {
+            //IConfiguration configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json", false, true)
+            //    .Build();
+
+            //optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
             optionsBuilder.UseSqlServer(
-                @"Server=127.0.0.1;Database=geo-pet;User=SA;Password=password12!;TrustServerCertificate=True");
+                @"Server=127.0.0.8;Database=GeoPetDb;User=root;Password=password12!;Trusted_Connection=True;MultipleActiveResultSets=True"
+               );
         }
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasData(
+                new User { UserId = 1, Name = "Adriano", Email = "teste1@teste.com.br", Cep = "18037-300", Password = "123456" },
+                new User { UserId = 2, Name = "Eduardo", Email = "teste2@teste.com.br", Cep = "18037-310", Password = "654321" },
+                new User { UserId = 3, Name = "Astolfo", Email = "teste3@teste.com.br", Cep = "18037-320", Password = "321654" }
+            );
+
+        modelBuilder.Entity<Pet>()
+            .HasData(
+                new Pet { PetId = 1, Size = "Large", Age = 4, Breed = BreedEnum.Doberman, Name = "Pequenino", UserId = 3 },
+                new Pet { PetId = 2, Size = "Small", Age = 2, Breed = BreedEnum.Pinscher, Name = "Zangado", UserId = 2 },
+                new Pet { PetId = 3, Size = "Medium", Age = 3, Breed = BreedEnum.Pit_Bull, Name = "Pandora", UserId = 1 }
+            );
+    }
 }
+
+// @"Server=127.0.0.1;Database=geopetdb;User=root;Password=password12!;TrustServerCertificate=True"
+// "Server=(localdb)\\msqllocaldb;Database=GeoPetDb;Trusted_Connection=True;MultipleActiveResultSets=True"
+// "Server=(localdb)\\msqllocaldb;Database=GeoPetDb;User=root;Password=password12!;Trusted_Connection=True;MultipleActiveResultSets=True
